786d30117fe70af35f406cb0bb03ad14ddff06f6
Anno! <79550877+Sir-Anno@users.noreply.github.com>
2024-03-02 15:53:21 +0000

    UE init

---
+++ Source/UnrealEngine_Demos/UnrealEngine_Demos.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealEngine_Demos.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealEngine_Demos, "UnrealEngine_Demos" );
