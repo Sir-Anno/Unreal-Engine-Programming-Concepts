ae9a2d4fdf116571dc693146c7e7a8fa2b69909b
Anno! <79550877+Sir-Anno@users.noreply.github.com>
2024-03-09 19:22:02 +0000

    SimpleDoor

---
+++ Source/UnrealEngine_Demos/Doors/SimpleDoor.cpp
@@ -0,0 +1,154 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SimpleDoor.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Character.h"
+#include "Components/TimelineComponent.h"
+#include "Net/UnrealNetwork.h"
+
+// Sets default values
+ASimpleDoor::ASimpleDoor()
+{
+	// Set this actor to call Tick() every frame
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Set is replicates
+	bReplicates = true;
+
+	// Create default scene root
+	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>("DefaultSceneRoot");
+	SetRootComponent(DefaultSceneRoot);
+
+	// Create pivot scene component
+	Pivot = CreateDefaultSubobject<USceneComponent>("Pivot");
+	Pivot->SetupAttachment(RootComponent);
+	Pivot->bVisualizeComponent = true;
+
+	// Create door frame mesh
+	DoorFrameMesh = CreateDefaultSubobject<UStaticMeshComponent>("DoorFrameMesg");
+	DoorFrameMesh->SetupAttachment(RootComponent);
+
+	// Create and attach door mesh to pivot
+	DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>("DoorMesh");
+	DoorMesh->SetupAttachment(Pivot);
+
+	// Create collision box
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>("BoxCollision");
+	CollisionBox->SetupAttachment(RootComponent);
+}
+
+// Begin play
+void ASimpleDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &ASimpleDoor::BeginCollisionOverlap);
+	CollisionBox->OnComponentEndOverlap.AddDynamic(this, &ASimpleDoor::EndCollisionOverlap);
+
+	if (OpenCurve)
+	{
+		FOnTimelineFloat TimelineProgress;
+		TimelineProgress.BindUFunction(this, FName("OpenUpdate"));
+
+		OpenTimeline.AddInterpFloat(OpenCurve, TimelineProgress);
+	}
+}
+
+// Tick
+void ASimpleDoor::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	OpenTimeline.TickTimeline(DeltaSeconds);
+}
+
+// Replicated properties
+void ASimpleDoor::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ASimpleDoor, FOpenAngle)
+	DOREPLIFETIME(ASimpleDoor, bIsOpen)
+}
+
+// Box collision begin overlap
+void ASimpleDoor::BeginCollisionOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	// Check is server authoritative 
+	if (!HasAuthority())
+	{
+		return;
+	}
+
+	// Check is character
+	if (!Cast<ACharacter>(OtherActor))
+	{
+		return;
+	}
+
+	// Compare doors direction to players direction 
+	if (GetActorForwardVector().Equals(OtherActor->GetActorForwardVector(), 0.5f))
+	{
+		FOpenAngle.Yaw = 90.0f;
+	}
+	else
+	{
+		FOpenAngle.Yaw = -90.0f;
+	}
+
+	bIsOpen = true;
+	OnRep_IsOpen(); // Manually call RepNotify as "ReplicatedUsing" function is not automatically called for server
+}
+
+// Box collision end overlap
+void ASimpleDoor::EndCollisionOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	// Check is server authoritative 
+	if (!HasAuthority())
+	{
+		return;
+	}
+
+	// Check is character
+	if (!Cast<ACharacter>(OtherActor))
+	{
+		return;
+	}
+
+	bIsOpen = false;
+	OnRep_IsOpen(); // Manually call RepNotify as "ReplicatedUsing" function is not automatically called for server
+}
+
+// bIsOpen RepNotify
+void ASimpleDoor::OnRep_IsOpen()
+{
+	if (bIsOpen)
+	{
+		OpenDoor();
+	}
+	else
+	{
+		CloseDoor();
+	}
+}
+
+// Update door rotation
+void ASimpleDoor::OpenUpdate(float Alpha)
+{
+	FQuat AQuat(FCloseAngle);
+	FQuat BQuat(FOpenAngle);
+	
+	Pivot->SetRelativeRotation(FQuat::Slerp(AQuat, BQuat, Alpha));
+}
+
+// Play open door timeline
+void ASimpleDoor::OpenDoor()
+{
+	OpenTimeline.Play();
+}
+
+// Reverse open door timeline
+void ASimpleDoor::CloseDoor()
+{
+	OpenTimeline.Reverse();
+}
---
+++ Source/UnrealEngine_Demos/Doors/SimpleDoor.h
@@ -0,0 +1,89 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/TimelineComponent.h"
+#include "SimpleDoor.generated.h"
+
+class UBoxComponent;
+class UCurveFloat;
+
+UCLASS()
+class UNREALENGINE_DEMOS_API ASimpleDoor : public AActor
+{
+	GENERATED_BODY()
+
+	// Default scene root
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USceneComponent> DefaultSceneRoot;
+	
+	// Pivot
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USceneComponent> Pivot;
+
+	// Door frame mesh
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> DoorFrameMesh;
+
+	// Door mesh
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> DoorMesh;
+
+	// Collision box
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UBoxComponent> CollisionBox;
+
+	// Is open
+	UPROPERTY(ReplicatedUsing = OnRep_IsOpen, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	bool bIsOpen;
+
+	// Rotation amount
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FRotator FCloseAngle;
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FRotator FOpenAngle;
+
+	// Curve float
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCurveFloat> OpenCurve;
+
+	// Timeline
+	FTimeline OpenTimeline;
+
+public:
+	// Sets default values for this actor's properties
+	ASimpleDoor();
+	
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const override;
+
+protected:
+	// Box collision begin overlap
+	UFUNCTION()
+	void BeginCollisionOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	
+	// Box collision end overlap
+	UFUNCTION()
+	void EndCollisionOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	// bIsOpen RepNotify
+	UFUNCTION()
+	void OnRep_IsOpen();
+
+	// Timeline update
+	UFUNCTION()
+	void OpenUpdate(float Alpha);
+
+	// Open door
+	UFUNCTION()
+	void OpenDoor();
+
+	// Close door
+	UFUNCTION()
+	void CloseDoor();
+};
