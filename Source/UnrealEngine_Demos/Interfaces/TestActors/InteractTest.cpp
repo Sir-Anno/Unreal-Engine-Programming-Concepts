b6804dd93fc22cfd0dc3bedb8e773451ea5c4d0b
Anno! <79550877+Sir-Anno@users.noreply.github.com>
2024-03-28 15:37:29 +0000

    Updates to interact interface

---
+++ Source/UnrealEngine_Demos/Interfaces/TestActors/InteractTest.cpp
@@ -0,0 +1,16 @@
+﻿#include "InteractTest.h"
+
+
+// Sets default values
+AInteractTest::AInteractTest()
+{
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh");
+}
+
+// Interact interface
+void AInteractTest::Interact()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, TEXT("Hello, I am a cube :D"));
+}
+
+
---
+++ Source/UnrealEngine_Demos/Interfaces/TestActors/InteractTest.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "UnrealEngine_Demos/Interfaces/InteractInterface.h"
+#include "InteractTest.generated.h"
+
+UCLASS()
+class UNREALENGINE_DEMOS_API AInteractTest : public AActor, public IInteractInterface
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	TObjectPtr<UStaticMeshComponent> StaticMesh;
+
+public:
+	// Sets default values for this actor's properties
+	AInteractTest();
+
+	virtual void Interact() override;
+};
