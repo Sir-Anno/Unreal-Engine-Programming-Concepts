08c38f8e47787555e965e3b5452b2aa5dd11bc0d
Anno! <79550877+Sir-Anno@users.noreply.github.com>
2024-04-03 20:56:54 +0100

    Color cube

---
+++ Source/UnrealEngine_Demos/Interactables/ColorCube/ColorCube.cpp
@@ -0,0 +1,55 @@
+﻿#include "ColorCube.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Net/UnrealNetwork.h"
+
+// Sets default values
+AColorCube::AColorCube()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Enable replication
+	bReplicates = true;
+
+	// Static mesh
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh");
+	StaticMesh->ComponentTags.Add("PlayerInteractable");
+	RootComponent = StaticMesh;
+}
+
+// Begin play
+void AColorCube::BeginPlay()
+{
+	Super::BeginPlay();
+
+	MaterialInstance = StaticMesh->CreateDynamicMaterialInstance(0, StaticMesh->GetMaterial(0));
+}
+
+// Replicated properties
+void AColorCube::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AColorCube, Color)
+}
+
+// Interact interface
+void AColorCube::Interact()
+{
+	// Check is server authoritative
+	if(!HasAuthority()) return;
+	
+	SetColor();
+	OnRep_Color(); // Manually call RepNotify as "ReplicatedUsing" function is not automatically called for server
+}
+
+void AColorCube::SetColor()
+{
+	Color =  FLinearColor(UKismetMathLibrary::HSVToRGB(UKismetMathLibrary::RandomFloatInRange(0, 359), 1, 1, 1));
+}
+
+// Color RepNotify
+void AColorCube::OnRep_Color()
+{
+	MaterialInstance->SetVectorParameterValue("Color", Color);
+}
---
+++ Source/UnrealEngine_Demos/Interactables/ColorCube/ColorCube.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "UnrealEngine_Demos/Interfaces/InteractInterface.h"
+#include "ColorCube.generated.h"
+
+UCLASS()
+class UNREALENGINE_DEMOS_API AColorCube : public AActor, public IInteractInterface
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> StaticMesh;
+
+	UPROPERTY()
+	TObjectPtr<UMaterialInterface> Material;
+
+	UPROPERTY()
+	TObjectPtr<UMaterialInstanceDynamic> MaterialInstance;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Color)
+	FLinearColor Color;
+
+public:
+	// Sets default values for this actor's properties
+	AColorCube();
+
+	virtual void BeginPlay() override;
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	virtual void Interact() override;
+
+protected:
+	void SetColor();
+
+	UFUNCTION()
+	void OnRep_Color();
+};
